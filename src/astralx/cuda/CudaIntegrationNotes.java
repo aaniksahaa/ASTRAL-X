fb21bef5d6846fe614974bf7fb8ebc2eccb51b7c
Anik Saha <aaniksahaa.2001@gmail.com>
2026-03-26 16:09:08 +0600

    src clean

--- src/astralx/cuda/CudaIntegrationNotes.java
+++
@@ -1,9 +0,0 @@
-package astralx.cuda;
-
-public final class CudaIntegrationNotes {
-    private CudaIntegrationNotes() {}
-
-    public static String note() {
-        return "CUDA kernels are in src/cuda/. Integrate via JNI or JavaCPP for GPU hash/bin lookups and batched scoring.";
-    }
-}
